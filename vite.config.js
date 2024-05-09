33695d34b2e23aa2d62ff48a5c1cab04bcacc427
Md. Samiul Islam <samiul.primary@gmail.com>
2024-05-09 20:28:29 +0600

    base fixed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nextgram/"
+  base: "/nextgram/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
