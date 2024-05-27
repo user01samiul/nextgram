4e1e270e2b90ef9d41c53800fb0eab2446c95017
Md. Samiul Islam <samiul.primary@gmail.com>
2024-05-27 08:44:31 +0600

    css js

--- src/auth/PublicLayout.jsx
+++ src/auth/PublicLayout.jsx
@@ -1,14 +1,32 @@
 import { useAuth } from "@/contexts/AuthContext";
-import { Navigate, Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Navigate } from "react-router-dom";
 
-function PublicLayout({children}) {
+function PublicLayout({ children }) {
   const { user } = useAuth();
+
+  useEffect(() => {
+    const handleResize = () => {
+      document.documentElement.style.setProperty(
+        "--vh",
+        `${window.innerHeight * 0.01}px`
+      );
+    };
+
+    handleResize(); // Set initial value
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
       {user ? (
         <Navigate to="/" />
       ) : (
-        <section className=" w-full h-screen flex flex-row overflow-hidden">
+        <section className=" w-full publicLayout flex flex-row overflow-hidden">
           <div className=" formSection w-full lg:w-[50%] h-full flex items-center">
             {/* <Outlet /> */}
             {children}
